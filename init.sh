95f609ab7b246f3a716b1ae459928290486b63ef
Juan Carielo <juancarielo@gmail.com>
2020-01-27 16:24:11 -0300

    A simple bash to create the environment

--- init.sh
+++ init.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-# This script just works on the Linux!
-
-# create and activate the virtualenv
+# Create and activate the virtualenv
+rm -rf venv
 python3 -m venv venv
 source venv/bin/activate
+
+# Install requirements
+pip install -r requirements.txt
+
+# Run migrations
+cd pyfolio
+python manage.py makemigrations
+python manage.py migrate
+
+# Run server
+python manage.py runserver
